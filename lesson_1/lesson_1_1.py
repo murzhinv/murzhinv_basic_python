a2ad09e981b347a23ef02ba674bad11d88e6363d
nmurzhi <nmurzhi@click2.money>
2025-10-07 15:21:41 +0300

    homework_1_1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,30 @@
-print("Привет, мир!")
+# a = 7
+# b = 8.34
+# str_1 = "Легко в IT"
+# b = a
+# a = 8
+# print(b)
+
+# a = b = c = 0
+# a, b, c = 0, 0, 0
+
+# a, b = 8, 10
+# a, b = b, a
+# print(a)
+# print(b)
+
+# print(type(a))
+# print(type(b))
+# print(type(str_1))
+
+# message = "Привет, Николай"
+# count = 0
+# Count = 0
+
+a = c = 4
+b, d = "Какая-нибудь строка", "Какая-нибудь  строка"
+
+print(id(a))
+print(id(c))
+print(id(b))
+print(id(d))
