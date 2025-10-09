86b7df588e062af25494ab091395d423d1422bd0
nmurzhi <nmurzhi@click2.money>
2025-10-09 10:50:07 +0300

    homework_3

---
+++ lesson_3/lesson_3_1.py
@@ -0,0 +1,23 @@
+# a = "string"
+# print(34, a)
+# print(34, 432, "ertyyh", a)
+
+# print(2 + 2 * 2)
+
+# a = 12
+# b = 3
+# c = 89
+
+# print(a, b, c, sep=" ", end="\n")
+# print(a, b, c, sep=" | ")
+# print("Результат умножения числа", a, "и числа", b, "равен", a * b)
+# print(f"Результат умножения числа {a} и числа {b} равен {a * b}") # f-string
+
+# a = float(input())
+# print(type(a))
+# print(a)
+
+a = float(input("Введите длину прямоугольника: "))
+b = float(input("Введите ширину прямоугольника: "))
+
+print(f"Периметр: {2 * (a + b)}")
---
+++ lesson_3/lesson_3_2.py
@@ -0,0 +1,33 @@
+# print(4 > 2)
+# print(4 > 9)
+# a = 11
+# b = 10
+# result = a >= b
+# print(type(result)
+
+# print(5 <= 7 - 2)
+# a = "asd"
+# b = "asd"
+# print(a != b)
+
+# x = 20
+# print(x % 2 == 0)
+
+# x = 27
+# print(x % 3 == 0) # наше число кратно 3
+
+# a = 1.987
+# # print(a >= 1 and a <= 1.986)
+# print(1 <= a <= 2)
+# name = "Николай"
+# print(("Н" in name) or ("я" in name))
+
+# x = 5
+# print(not ((x % 2 == 0) or (x % 3 == 0)))
+a = 34321
+print(bool(1))
+print(bool(0))
+print(bool(-11))
+print(bool("qwert"))
+print(bool(""))
+print(bool(a))
